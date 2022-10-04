4ca4af6c3a3d3da1ef091e2bf3d80f7d90dab9f7
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-04 21:07:05 +0300

    add new example in lesson_4

---
+++ lesson_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(1,1000);
+int b = new Random().Next(1,1000);
+int c = new Random().Next(1,1000);
+int d = new Random().Next(1,1000);
+int e = new Random().Next(1,1000);
+int max = a;
+
+if (b > max) max  = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e);
+Console.Write(max);
