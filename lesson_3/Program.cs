1e9bb1a798641f990a41c1b89d6b18f41473de60
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-04 20:53:02 +0300

    add lesson_3 example

--- lesson_3/Program.cs
+++ lesson_3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter your name ");
+string user = Console.ReadLine();
+
+if (user.ToLower() == "Andrei")
+{
+     Console.WriteLine("Здорова, отец");
+}
+else 
+{
+Console.WriteLine("Sosi");
+}
